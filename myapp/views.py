c24ce5992cffb080693efd7ecfde4367e62f989e
MaxiAgrippa <maxiagrippamail@mail.com>
2020-10-22 11:05:06 -0700

    Lab05

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from .models import Topic, Course, Student, Order
+
 
 # Create your views here.
+def index(request):
+    top_list = Topic.objects.all().order_by('id')[:10]
+    course_list = Course.objects.all().order_by('-title')[:5]
+    response = HttpResponse()
+    heading1 = '<p>' + 'List of topics: ' + '</p>'
+    response.write(heading1)
+    for topic in top_list:
+        para = '<p>' + str(topic.id) + ': ' + str(topic) + '</p>'
+        response.write(para)
+    heading1 = '<p>' + 'List of courses: ' + '</p>'
+    for course in course_list:
+        para = '<p> Course title: ' + str(course.title) + '; Price: ' + str(course.price) + '</p>'
+        response.write(para)
+    return response
+
+
+def about(request):
+    response = HttpResponse()
+    para = '<p>This is an E-learning Website! Search our Topics to find all available Courses.</p>'
+    response.write(para)
+    return response
+
+
+def detail(request, topic_id):
+    topic_local = get_object_or_404(Topic, pk=topic_id)
+    courses = Course.objects.filter(topic=topic_id)
+    response = HttpResponse()
+    para = '<p>' + topic_local.name.upper() + '<p>'
+    response.write(para)
+    para = '<p> ' + str(topic_local.length) + ' weeks <p>'
+    response.write(para)
+    for course in courses:
+        para = '<p> Course title: ' + str(course.title) + '</p>'
+        response.write(para)
+    return response
--- mysiteF20/settings.py
+++ mysiteF20/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '9l4#wbn8np+8eor*zu#x=!8xq=p@q(ix_78@g#l4)@a%8#h414'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
